f16333eb2ccedaefadd3019009dd9045d8072563
SirawichDev <sirawit0676@gmail.com>
2020-03-19 17:24:04 +0700

    do Provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import {TodoProvider} from './to-do-context'
+import * as serviceWorker from "./serviceWorker";
+import { InputTask } from "./components/input-task";
+import { TaskList } from "./components/task-list";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <TodoProvider>
+    <InputTask/>
+    <TaskList/>
+  </TodoProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
